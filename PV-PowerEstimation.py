ab2ff316507e30710d13752da915813a50da5be4
Secwest.net <70259601+secwest@users.noreply.github.com>
2025-07-06 18:33:05 -0700

    Remove Dup Header

--- PV-PowerEstimation.py
+++ PV-PowerEstimation.py
@@ -2,24 +2,6 @@
 # -*- coding: utf-8 -*-
 
 """
-PV-PowerEstimate.py - Solar PV Power Yield Calculator & Tutorial with Incentives v1.3.1
-
-Copyright (c) 2025, Dragos Ruiu
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
 PV-PowerEstimate.py - Solar PV Power Yield Calculator & Tutorial with Incentives v1.3.2
 
 Copyright (c) 2025, Dragos Ruiu
